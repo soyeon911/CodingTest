4f37cc8199488d80a7ba596ce70771c9ca21c20b
soyeon911 <sypark911@yonsei.ac.kr>
2025-04-26 07:45:10 +0900

    0426

--- silver/터렛.py
+++ silver/터렛.py
@@ -1,10 +1,12 @@
 import math
-x1, y1, r1, x2, y2, r2 = input().split()
+T = int(input())
 
 def turret(x1, y1, r1, x2, y2, r2):
-    d = math.hypot(x2-x1, y2-y1)
+    d = math.hypot(x2 - x1, y2 - y1)
 
-    if d == r1 + r2:
+    if x1 == x2 and y1 == y2 and r1 == r2:
+        return -1
+    elif d == r1 + r2:
         return 1
     elif d == abs(r1 - r2):
         return 1
@@ -12,7 +14,15 @@ def turret(x1, y1, r1, x2, y2, r2):
         return 2
     elif d > (r1 + r2):
         return 0
-    else:  
+    else:
         return 0
+
+results = []
+
+# T번 반복해서 입력 받기
+for _ in range(T):
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
+    results.append(turret(x1, y1, r1, x2, y2, r2))
     
-turret(x1, y1, r1, x2, y2, r2)
+for result in results:
+    print(result)
