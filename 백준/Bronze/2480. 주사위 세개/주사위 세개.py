1e9f7b673d277d131bc7325a0b3f0538c1b510ff
soyeon <163836036+soyeon911@users.noreply.github.com>
2025-07-11 20:20:59 +0900

    [Bronze IV] Title: 주사위 세개, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,15 +1,12 @@
 a, b, c = map(int, input().split())
 
 def dice(a, b, c):
-    if(a == b == c):
-        return 10000 + a*1000
-    if(a == b != c):
-        return 1000 + a*100
-    if(a != b == c):
+    if a == b == c:
+        return 10000 + a * 1000
+    elif a == b or a == c:
+        return 1000 + a * 100
+    elif b == c:
         return 1000 + b * 100
-    if(a == c != b):
-        return 1000 + a* 100
-    if(a != b != c):
-        return max(a, b, c)*100
-    
+    else:
+        return max(a, b, c) * 100
 print(dice(a, b, c))
